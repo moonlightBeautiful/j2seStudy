7f448f984dff7be815e9af2c66cf2069a746221e
agaoxu <xy20539@163.com>
2019-05-30 17:40:14 +0800

    Initial commit

---
+++ src/main/java/com/java1234/c12thread/c01noThread/App.java
@@ -0,0 +1,13 @@
+package com.java1234.c12thread.c01noThread;
+
+/**
+ * @author gaoxu
+ * @date 2019-05-30 17:37
+ * @description ... 类
+ */
+public class App {
+    public static void main(String[] args) {
+        new Eat().eatFood();
+        new Music().listenMusic();
+    }
+}
